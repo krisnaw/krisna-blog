290532d075f94f8014a1942451328c8db1223672
krisna <krisna.w2010@gmail.com>
2025-03-20 06:34:18 +0800

    Add theme toggle, marquee, and animations to UI components
    Replaced `ModeToggle` with `ThemeToggle` in the header for better theme-switching functionality. Introduced `Marquee` and `BlurFade` components to enhance UI animations and improve visual appeal. Updated global styles to include new color variables for consistency.

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 import {Logo} from "@/components/logo";
-import {ModeToggle} from "@/components/mode-toggle";
 import Link from "next/link";
+import {ThemeToggle} from "@/components/theme-toggle";
 
 export default function Header() {
     return (
@@ -9,7 +9,7 @@ export default function Header() {
                 <div className="flex items-center w-full">
                     <Logo/>
                 </div>
-                <div className="flex items-center gap-x-12">
+                <div className="flex items-center gap-x-4">
                     <div>
                         <Link href="/about"
                               className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] rounded-sm p-2 transition-[background-color]">
@@ -17,7 +17,7 @@ export default function Header() {
                         </Link>
                     </div>
                     <div>
-                        <ModeToggle/>
+                        <ThemeToggle/>
                     </div>
                 </div>
             </nav>
