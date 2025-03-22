26107da710a26474693759f01d1cfd765ded5a08
krisna <krisna.w2010@gmail.com>
2025-03-22 15:46:24 +0800

    Update about page layout and remove unused Bio component
    Replaced the `Bio` component with inline content for better customization. Expanded the about page with new sections, including specialized areas, work experience, and project descriptions. Adjusted layout container size to `max-w-3xl` for improved aesthetics.

--- app/(home)/about/page.tsx
+++ app/(home)/about/page.tsx
@@ -1,7 +1,115 @@
-import Bio from "@/components/bio";
+import {BlurFade} from "@/components/ui/blur-fade";
+import {Button} from "@/components/ui/button";
+import Image from "next/image";
 
 export default function About() {
     return (
-        <Bio />
+        <div className="space-y-10">
+
+            <section>
+
+                <div className="flex">
+
+                    <div className="mr-6 shrink-0">
+                        <Image
+                            alt=""
+                            src="/images/profiles.jpg"
+                            width={200}
+                            height={200}
+                            className="inline-block size-36 rounded-full"
+                        />
+                    </div>
+
+                    <div>
+                        <BlurFade delay={0.25} inView>
+                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                                Hello... 👋
+                            </h2>
+                        </BlurFade>
+                        <BlurFade delay={0.25 * 2} inView>
+                            <span className="text-xl text-pretty tracking-tighter sm:text-3xl xl:text-4xl/none">
+                                I&#39;m Krisna Wijaya, Fullstack developer.
+                            </span>
+                        </BlurFade>
+                        <BlurFade delay={0.25 * 2} inView>
+                            <span className="text-xl text-pretty tracking-tighter sm:text-2xl">
+                                Nice to meet you
+                            </span>
+                        </BlurFade>
+                    </div>
+
+
+                </div>
+
+
+                <div className="mt-10">
+                    <h5 className="text-4xl font-extrabold text-gray-900">
+                        A design that connects AI technology and user experience
+                    </h5>
+                    <p className="mt-5 text-lg text-gray-600">
+                        Hello! En. It is. We are focusing on MVP development that hypothesisifies the user experience of the new era based on AI at high speed.
+                    </p>
+                </div>
+
+                <div className="mt-10 flex flex-col-reverse md:flex-row gap-3">
+                    <Button className="w-1/2" size="lg" variant="secondary">Inquire</Button>
+                    <Button className="w-1/2" size="lg">Make an appoinment</Button>
+                </div>
+
+            </section>
+
+            <section>
+                <div>
+                    <h5 className="text-2xl font-bold mb-5">Work experience</h5>
+                </div>
+
+                <div className="space-y-6">
+                    <div>
+                        <div className="text-black/50 text-sm mb-1">2024 - Present</div>
+                        <h3 className="text-base font-medium mb-3">AI-UX Designer &amp; Engineer at</h3>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="flex flex-col gap-2 mb-5 px-1">
+                    <h5 className="text-2xl font-bold mb-5">Specialized Area</h5>
+                    <p className="text-base text-gray-600">
+                        I am good at designing new user experiences based on AI technology and developing MVPs that verify their hypothethes as quickly as possible.
+                    </p>
+                </div>
+                <div className="space-y-4">
+                    <div className="rounded-3xl border text-card-foreground shadow-sm overflow-hidden bg-[#F6F5F4]">
+                        <div className="p-8 px-5 py-4"><h3 className="font-bold">AI</h3><p
+                            className="text-sm text-gray-600">Designing workflows using LLM, customization of Diffusion
+                            models</p></div>
+                    </div>
+
+                    <div className="rounded-3xl border text-card-foreground shadow-sm overflow-hidden bg-[#F6F5F4]">
+                        <div className="p-8 px-5 py-4"><h3 className="font-bold">AI</h3><p
+                            className="text-sm text-gray-600">Designing workflows using LLM, customization of Diffusion
+                            models</p></div>
+                    </div>
+
+                    <div className="rounded-3xl border text-card-foreground shadow-sm overflow-hidden bg-[#F6F5F4]">
+                        <div className="p-8 px-5 py-4"><h3 className="font-bold">AI</h3><p
+                            className="text-sm text-gray-600">Designing workflows using LLM, customization of Diffusion
+                            models</p></div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div className="flex flex-col gap-2 mb-5 px-1">
+                    <h5 className="text-2xl font-bold mb-5">Project</h5>
+                    <p className="text-base text-gray-600">
+                        We will introduce some of the participating projects
+                    </p>
+                </div>
+
+            </section>
+
+
+        </div>
     )
 }
--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -6,7 +6,7 @@ export default async function HomeLayout(
 ) {
 
     return (
-        <div className="max-w-2xl m-auto">
+        <div className="max-w-3xl m-auto">
             <main className="p-6 pt-3 md:pt-6 min-h-screen flex flex-col">
                 <Header />
                 {children}
