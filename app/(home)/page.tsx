87fb9373646fdefea9f3389591c2d8d0ca99d468
krisna <krisna.w2010@gmail.com>
2025-03-29 12:17:05 +0800

    Update posts fetching logic with `unstable_cache`
    Replaced `getPosts` with a new `unstable_cache` implementation to optimize data fetching and caching for posts. Removed unused static generation functions in the post detail page to simplify and improve performance.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,31 @@
 import PostsListView from "@/components/post/posts-list-view";
-import {getPosts} from "@/app/actions/post-action";
 import type {Metadata} from 'next'
+import {unstable_cache} from "next/cache";
+import {createClient} from "@/utils/supabase/client";
 
 export const metadata: Metadata = {
     title: 'Krisna Wijaya',
     description: 'An overview of my career, writing and example projects',
 }
 
+const getPostsList = unstable_cache(
+    async () => {
+        const supabase = await createClient();
+        const { data: posts } = await  supabase.from('posts')
+            .select('*')
+
+            .order('created_at', { ascending: false })
+
+        return posts;
+    },
+    ['posts'],
+    { revalidate: 3600, tags: ['posts'] }
+)
+
+
 export default async function Home() {
 
-    const posts = await getPosts();
+    const posts = await getPostsList();
 
     return (
         <div>
--- app/(home)/post/[slug]/page.tsx
+++ app/(home)/post/[slug]/page.tsx
@@ -2,25 +2,6 @@ import {createClient} from "@/utils/supabase/client";
 import markdownHtml from 'zenn-markdown-html';
 import {PostHeader} from "@/components/post/post-header";
 
-// Next.js will invalidate the cache when a
-// request comes in, at most once every 60 seconds.
-export const revalidate = 60
-
-
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
-export const dynamicParams = true // or false, to 404 on unknown paths
-
-export async function generateStaticParams() {
-    const supabase = await createClient();
-    const { data: posts } = await supabase.from('posts').select('slug')
-
-    return posts?.map(({ slug }) => ({
-        slug,
-    }))
-}
-
 export default async function Page({
                                        params,
                                    }: {
