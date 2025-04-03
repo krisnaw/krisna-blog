a2d18c677d8ffdf70de81358007feb941052fdf6
krisna <krisna.w2010@gmail.com>
2025-04-03 22:57:37 +0800

    Enhance styling and update content across home and footer
    Updated layout dimensions, text styles, and image sizes to improve design responsiveness and readability. Added new Roboto Mono font to the footer and streamlined footer content for clarity. Introduced additional visual elements in the project showcase section.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -18,22 +18,21 @@ export default async function Home() {
     return (
         <div>
 
-            <div className="max-w-3xl m-auto py-32 min-h-screen">
+            <div className="max-w-5xl m-auto py-32 min-h-screen">
 
                 <div className="flex items-center mt-10">
-
                     <div>
                         <Image
                             className="rounded-lg grayscale hover:grayscale-0"
-                            src="/images/profiles.jpg" alt="Profile" width={180} height={180} />
+                            src="/images/profiles.jpg" alt="Profile" width={240} height={240} />
                     </div>
 
                     <div className="ml-10 mx-auto">
                         <BlurFade delay={0.25 * 2} inView>
-                            <h4 className={`text-5xl font-semibold mb-1.5 ${funnel.className}`}>
+                            <h4 className={`text-6xl font-semibold mb-1.5 ${funnel.className}`}>
                                 Full-Stack  <br/> Product Developer
                             </h4>
-                            <p className={`text-sm italic ${funnel.className}`}>
+                            <p className={`text-base italic leading-relaxed ${funnel.className}`}>
                                 Emphasizes end-to-end ownership of a product—from backend logic to frontend polish
                             </p>
                         </BlurFade>
@@ -43,41 +42,66 @@ export default async function Home() {
 
             </div>
 
-            <section className="max-w-5xl px-6 mx-auto pb-32 bg-gray-100">
-                <div className="flex justify-between items-center">
-                    <div className={`text-6xl font-bold ${funnel.className}`}>
-                        What&#39;s a Product Engineering?
-                    </div>
-                    <div className="max-w-lg space-y-6">
-                        <p className={`leading-relaxed text-2xl/6 ${funnel.className}`}>
-                            As a Product Engineer, I blend the problem-solving mindset of a product builder with the technical expertise of a full-stack developer.
-                        </p>
+            <section className="bg-gray-100 rounded-lg py-32">
+                <div className="max-w-5xl mx-auto">
+                    <div className="flex justify-between items-center gap-10">
+                        <div className={`text-6xl font-bold ${funnel.className}`}>
+                            What&#39;s a Product Engineering?
+                        </div>
+                        <div className="max-w-lg space-y-6">
+                            <p className={`leading-relaxed text-2xl/6 ${funnel.className}`}>
+                                As a Product Engineer, I blend the problem-solving mindset of a product builder with the technical expertise of a full-stack developer.
+                            </p>
 
-                        <p className={`leading-relaxed text-2xl/6 ${funnel.className}`}>
-                            This enables me to transform ideas into fully functional products,
-                            delivering end-to-end solutions—coding frontends, backends,
-                            and everything in between—while prioritizing user needs, scalability, and fast, reliable launches.
-                        </p>
+                            <p className={`leading-relaxed text-2xl/6 ${funnel.className}`}>
+                                This enables me to transform ideas into fully functional products,
+                                delivering end-to-end solutions—coding frontends, backends,
+                                and everything in between—while prioritizing user needs, scalability, and fast, reliable launches.
+                            </p>
 
+                        </div>
                     </div>
                 </div>
-
             </section>
 
-            <div className="mt-20 px-8 rounded-lg py-16">
-                <div className="px-6">
-                    <div className="max-w-xl">
-                        <h4 className="text-4xl font-bold">
-                            Haup Car - Charter Services
-                        </h4>
-                        <p className="mt-5 text-lg font-light">
-                            Implemented Figma designs into a functional web application using React and Ant Design, ensuring seamless UI and API integration for Haup Car.
-                        </p>
+            <div className="mt-20 pt-24 pb-16 sm:pt-32 sm:pb-24 xl:pb-32">
+                <div className="pb-20 sm:pb-24 xl:pb-0">
+                    <div className="max-w-5xl mx-auto">
+                        <div className="max-w-xl">
+                            <h4 className="text-4xl font-bold">
+                                Haup Car - Charter Services
+                            </h4>
+                            <p className="mt-5 text-lg font-light">
+                                Implemented Figma designs into a functional web application using React and Ant Design, ensuring seamless UI and API integration for Haup Car.
+                            </p>
+                        </div>
                     </div>
 
                     <div className="mt-10">
                         <a href="/haup">
-                            <img className="object-cover h-80 w-full rounded-lg shadow-lg"
+                            <img className="object-cover h-96 w-full rounded-lg shadow-lg"
+                                 src="/images/template1.png" alt="Haup Car"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-20 pt-24 pb-16 sm:pt-32 sm:pb-24 xl:pb-32">
+                <div className="pb-20 sm:pb-24 xl:pb-0">
+                    <div className="max-w-5xl mx-auto">
+                        <div className="max-w-xl">
+                            <h4 className="text-4xl font-bold">
+                                Haup Car - Charter Services
+                            </h4>
+                            <p className="mt-5 text-lg font-light">
+                                Implemented Figma designs into a functional web application using React and Ant Design, ensuring seamless UI and API integration for Haup Car.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="mt-10">
+                        <a href="/haup">
+                            <img className="object-cover h-96 w-full rounded-lg shadow-lg"
                                  src="/images/template1.png" alt="Haup Car"/>
                         </a>
                     </div>
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,25 +1,21 @@
 import Image from "next/image";
+import {Roboto_Mono} from 'next/font/google'
+
+const roboto_mono = Roboto_Mono({
+    subsets: ['latin'],
+    variable: '--font-roboto-mono',
+    display: 'swap',
+})
 
 export default function Footer() {
     return (
-        <footer className=" border-t border-gray-200 dark:border-gray-700 py-6">
-            <div className="max-w-3xl m-auto">
+        <footer className={`border-t border-gray-200 dark:border-gray-700 py-6 ${roboto_mono.className}`}>
+            <div className="px-6">
                 <div className="flex justify-between">
-                    <a
-                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            aria-hidden
-                            src="/file.svg"
-                            alt="File icon"
-                            width={16}
-                            height={16}
-                        />
-                        Krisna Wijaya (@iKrisnaw)
-                    </a>
+                    <div className={` text-lg`}>
+                        Design and build by Krisna Wijaya
+                    </div>
+
                     <a
                         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
                         href="/used"
