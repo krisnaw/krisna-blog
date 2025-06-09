bfcdb4b7c1e1a0cac7dd63004fd1fc3dc20eb80c
krisna <krisna.w2010@gmail.com>
2025-06-09 21:16:19 +0800

    Refactor components and add navigation bar; implement loading skeletons

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <Disclosure as="nav" className="bg-white shadow-sm">
+        <Disclosure as="nav" className="bg-white shadow-sm sticky top-0 z-50">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
--- components/section/hero-section.tsx
+++ components/section/hero-section.tsx
@@ -10,7 +10,7 @@ const funnel = Funnel_Display({
 
 export default function HeroSection() {
     return (
-        <div className="max-w-5xl m-auto py-32 min-h-screen">
+        <div className="max-w-5xl m-auto py-32 min-h-[80vh]">
 
             <div className="flex items-center mt-10">
                 <div>
