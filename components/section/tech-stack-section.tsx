32472a9fa2c6e5bca04c59e194e6f0c7ce84f38d
krisna <krisna.w2010@gmail.com>
2025-06-11 14:40:16 +0800

    Add Supabase logo files and refactor tech stack section to use dynamic image rendering

--- components/section/tech-stack-section.tsx
+++ components/section/tech-stack-section.tsx
@@ -1,46 +1,50 @@
+import Image from "next/image";
+
+const tools = [
+    {
+        id: 1,
+        name: 'TriggerDev',
+        image_url: '/TriggerDev/trigger-dev-icon-light.svg'
+    },
+
+    {
+        id: 2,
+        name: 'Drizzle',
+        image_url: '/Drizzle/logo-github-sq-light.svg'
+    },
+
+    {
+        id: 3,
+        name: 'NEXTJS',
+        image_url: '/NEXTJS/logotype/light-background/nextjs-logotype-light-background.svg'
+    },
+
+    {
+        id: 4,
+        name: 'Vercel',
+        image_url: '/Vercel/logotype/light/vercel-logotype-light.svg'
+    },
+
+    {
+        id: 5,
+        name: 'Supabase',
+        image_url: '/Supabase/supabase-logo-wordmark--light.svg'
+    },
+]
+
 export default function TechStackSection() {
     return (
         <section id="tech-stack-section">
             <div className="py-24 sm:py-32">
                 <div className="mx-auto max-w-2xl lg:max-w-none">
                     <h2 className="text-lg/8 font-semibold text-gray-900">Tech Stack</h2>
-                    <div
-                        className="mx-auto mt-10 grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
-                        <img
-                            alt="Transistor"
-                            src="https://tailwindcss.com/plus-assets/img/logos/transistor-logo-gray-900.svg"
-                            width={158}
-                            height={48}
-                            className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-                        />
-                        <img
-                            alt="Reform"
-                            src={"/Drizzle/logo-github-sq-light.svg"}
-                            width={158}
-                            height={48}
-                            className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-                        />
-                        <img
-                            alt="NEXTJS"
-                            src={"/NEXTJS/logotype/light-background/nextjs-logotype-light-background.svg"}
-                            width={158}
-                            height={48}
-                            className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-                        />
-                        <img
-                            alt="Vercel"
-                            src={"/Vercel/logotype/light/vercel-logotype-light.svg"}
-                            width={158}
-                            height={48}
-                            className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-                        />
-                        <img
-                            alt="Supabase"
-                            src={"/Supabase/Supabase-logo-icon.svg"}
-                            width={158}
-                            height={48}
-                            className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
-                        />
+
+                    <div className="mx-auto mt-10 grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
+
+                        {tools.map((tool) => (
+                            <Image key={tool.id} src={tool.image_url} alt={tool.name}     width={158}
+                                   height={48} />
+                        ))}
                     </div>
                 </div>
             </div>
