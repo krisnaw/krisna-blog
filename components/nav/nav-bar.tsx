7ae5f5b1c961bb17fbff693331f951103b6d9713
krisna <krisna.w2010@gmail.com>
2025-06-17 15:26:19 +0800

    Update tech stack section; enhance NavBar component and improve Laravel logo SVG

--- components/nav/nav-bar.tsx
+++ components/nav/nav-bar.tsx
@@ -3,7 +3,6 @@
 import {HomeIcon} from "lucide-react";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {cn} from "@/lib/utils";
 import {ModeToggle} from "@/components/mode-toggle";
 import {Button} from "@/components/ui/button";
 
@@ -15,28 +14,28 @@ const navigation = [
 export default function NavBar() {
     const pathname = usePathname()
     const isActiveLink = /^\/.+$/.test(pathname);
+    console.log(pathname)
     return (
         <header className="fixed inset-x-0 top-0 z-50 w-full flex justify-center">
-            <nav aria-label="Global" className="flex items-center justify-between py-2.5 px-4 max-w-3xl w-full backdrop-blur-sm bg-white/70 border border-gray-200/50 rounded-full mt-6 shadow-sm">
+            <nav aria-label="Global" className="flex items-center justify-between py-2.5 px-4 max-w-3xl w-full backdrop-blur-sm 
+            bg-white/70 dark:bg-gray-900/70 border border-gray-200/50 dark:border-gray-800/50 rounded-full mt-6 shadow-sm">
 
                 <div className="flex lg:flex-1">
                     <Button variant="ghost" size="icon" asChild>
                         <Link href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Home</span>
-                            <HomeIcon className="size-6 text-gray-700" />
+                            <HomeIcon className="h-[1.2rem] w-[1.2rem]" />
                         </Link>
                     </Button>
                 </div>
 
                 <div className="lg:flex lg:gap-x-6">
                     {navigation.map((item) => (
-                        <a key={item.name} href={item.href}
-                           className={
-                            cn("text-sm/6 font-semibold text-gray-800 hover:text-gray-900 ",
-                            isActiveLink ? "bg-gray-100 rounded-full px-3 py-0.5": null)
-                           }>
-                            {item.name}
-                        </a>
+                        <Button key={item.name} variant={isActiveLink ? "secondary" : "ghost"}  asChild className="rounded-full">
+                            <Link  href={item.href}>
+                                {item.name}
+                            </Link>
+                        </Button>
                     ))}
                 </div>
 
--- components/section/tech-stack-section.tsx
+++ components/section/tech-stack-section.tsx
@@ -11,26 +11,44 @@ const tools = [
     },
 
     {
-        id: 2,
-        name: 'Drizzle',
-        image_dark: '/Drizzle/logo-github-sq-dark.svg',
-        image_light: '/Drizzle/logo-github-sq-light.svg',
+        id: 4,
+        name: 'Vercel',
+        image_dark: '/Vercel/logotype/dark/vercel-logotype-dark.svg',
+        image_light: '/Vercel/logotype/light/vercel-logotype-light.svg',
     },
+]
 
+const frameworks = [
     {
-        id: 3,
+        id: 1,
         name: 'NEXTJS',
         image_dark: '/NEXTJS/logotype/dark-background/nextjs-logotype-dark-background.svg',
         image_light: '/NEXTJS/logotype/light-background/nextjs-logotype-light-background.svg',
     },
 
     {
-        id: 4,
-        name: 'Vercel',
-        image_dark: '/Vercel/logotype/dark/vercel-logotype-dark.svg',
-        image_light: '/Vercel/logotype/light/vercel-logotype-light.svg',
+        id: 2,
+        name: 'Laravel',
+        image_dark: '/Laravel/laravel-logo.svg',
+        image_light: '/Laravel/laravel-logo.svg',
     },
 
+    {
+        id: 3,
+        name: 'ReactRouter',
+        image_dark: '/reactrouter/rr_lockup_dark.svg',
+        image_light: '/reactrouter/rr_lockup_light.svg',
+    },
+
+    {
+        id: 4,
+        name: 'NodeJS',
+        image_dark: '/nodejs/nodejsLight.svg',
+        image_light: '/nodejs/nodejsDark.svg',
+    },
+]
+
+const databases = [
     {
         id: 5,
         name: 'Supabase',
@@ -39,47 +57,70 @@ const tools = [
     },
 
     {
-        id: 6,
-        name: 'ReactRouter',
-        image_dark: '/reactrouter/rr_lockup_dark.svg',
-        image_light: '/reactrouter/rr_lockup_light.svg',
+        id: 2,
+        name: 'Drizzle',
+        image_dark: '/Drizzle/logo-github-sq-dark.svg',
+        image_light: '/Drizzle/logo-github-sq-light.svg',
     },
-    //
-    // {
-    //     id: 7,
-    //     name: 'NodeJS',
-    //     image_url: '/nodejs/nodejsDark.svg'
-    // },
-    //
-    // {
-    //     id: 8,
-    //     name: 'Laravel',
-    //     image_url: '/Laravel/laravel-logo.svg'
-    // },
-    //
-    // {
-    //     id: 9,
-    //     name: 'NodeJS',
-    //     image_url: '/nodejs/nodejsDark.svg'
-    // },
+
 ]
+
 
 export default function TechStackSection() {
     const theme = useTheme();
-    console.log(theme.theme)
     return (
         <section id="tech-stack-section">
             <div className="py-24 sm:py-32">
                 <div className="mx-auto max-w-2xl lg:max-w-none">
-                    <h2 className="text-lg/8 font-semibold ">Tech Stack</h2>
+                    <h2 className="text-xl/8 font-semibold ">Tech Stack</h2>
 
-                    <div className="mx-auto mt-10 grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:grid-cols-3">
-
-                        {tools.map((tool) => (
-                            <Image key={tool.id} src={theme.theme == 'dark' ? tool.image_dark: tool.image_light} alt={tool.name}     width={158}
-                                   height={48} />
-                        ))}
+                    <div className="mt-10">
+                        <h4 className="text-sm font-medium text-muted-foreground">Framework</h4>
+                        <div className="bg-gray-50 dark:bg-gray-900/50 rounded-lg p-6 mt-4">
+                            <div
+                                className="mx-auto grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:grid-cols-3">
+                                {frameworks.map((tool) => (
+                                    <Image key={tool.id}
+                                           src={theme.theme == 'dark' ? tool.image_dark : tool.image_light}
+                                           alt={tool.name} width={158}
+                                           height={48}/>
+                                ))}
+                            </div>
+                        </div>
                     </div>
+
+
+                    <div className="mt-10">
+                        <h4 className="text-sm font-medium text-muted-foreground">Database and ORM</h4>
+                        <div className="bg-gray-50 dark:bg-gray-900/50 rounded-lg p-6 mt-4">
+                            <div
+                                className="mx-auto grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:grid-cols-3">
+                                {databases.map((tool) => (
+                                    <Image key={tool.id}
+                                           src={theme.theme == 'dark' ? tool.image_dark : tool.image_light}
+                                           alt={tool.name} width={158}
+                                           height={48}/>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-10">
+                        <h4 className="text-sm font-medium text-muted-foreground">Infra and tools</h4>
+                        <div className="bg-gray-50 dark:bg-gray-900/50 rounded-lg p-6 mt-4">
+                            <div
+                                className="mx-auto grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:grid-cols-3">
+                                {tools.map((tool) => (
+                                    <Image key={tool.id}
+                                           src={theme.theme == 'dark' ? tool.image_dark : tool.image_light}
+                                           alt={tool.name} width={158}
+                                           height={48}/>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </section>
