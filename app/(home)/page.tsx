cd1cf33832dc7caa28347274ae51af5412ce81a1
krisna <krisna.w2010@gmail.com>
2025-09-09 19:40:54 +0800

    fix copy

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,15 @@
 import RecentProjectSection from "@/components/new-section/recent-project-section";
+import ExpSection from "@/components/new-section/exp-section";
+import ExploreSection from "@/components/new-section/explore-section";
 
 export default function Page() {
   return (
     <div>
       <RecentProjectSection/>
 
-      <section className="py-20 border-t border-gray-300">
-        <div className="max-w-xl">
-          <div>
-            <h1 className="uppercase text-7xl tracking-tighter text-balance">Recent Project.</h1>
-            <p className="font-light opacity-70 mt-2.5">
-              We&#39;ve been engaged in a variety of projects lately, crafting stories, designing websites, and developing digital experiences.
-              Our recent work reflects our commitment to helping clients achieve their goals.
-            </p>
-          </div>
-        </div>
-      </section>
+      <ExpSection />
 
+      <ExploreSection />
     </div>
   )
 }
---
+++ components/new-section/exp-section.tsx
@@ -0,0 +1,15 @@
+export default function ExpSection() {
+  return (
+    <section className="py-20 border-t border-gray-300">
+      <div className="max-w-xl">
+        <div>
+          <h1 className="uppercase text-7xl tracking-tighter text-balance">Experiences.</h1>
+          <p className="font-light opacity-70 mt-2.5">
+            We&#39;ve been engaged in a variety of projects lately, crafting stories, designing websites, and developing digital experiences.
+            Our recent work reflects our commitment to helping clients achieve their goals.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
