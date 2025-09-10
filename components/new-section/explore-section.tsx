f3759e21f11c4b588872bb20de6a0c1ac77ea5e2
krisna <krisna.w2010@gmail.com>
2025-09-10 17:16:30 +0800

    fix copy

--- components/new-section/explore-section.tsx
+++ components/new-section/explore-section.tsx
@@ -1,4 +1,5 @@
 import HeighAnimation from "@/components/animations/heigh-animation/height-anima";
+import {ButtonSendLink} from "@/components/animations/button-send-link/button-send-link";
 
 export default function ExploreSection() {
   return (
@@ -17,53 +18,51 @@ export default function ExploreSection() {
       </div>
 
 
-      <div className="mt-10">
+      <div className="mt-16">
 
-        <div className="grid grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
 
-          <div className="border border-gray-300 rounded-lg overflow-hidden">
+          <div className="divide-y divide-gray-200 overflow-hidden rounded-lg shadow-sm flex flex-col">
+            <div className="px-4 py-2.5 sm:px-6 flex justify-between items-center">
 
-            <div className="w-full flex justify-center items-center  rounded-t-lg py-6">
+              <div className="text-lg font-semibold text-muted-foreground">
+                Animation Height
+              </div>
+
+              <div className="shrink-0">
+                <img className="border border-gray-200 rounded-lg" width="45" height="45" alt="Motion logo" src="https://github.com/user-attachments/assets/00d6d1c3-72c4-4c2f-a664-69da13182ffc" />
+              </div>
+
+            </div>
+            <div className="px-4 py-5 sm:p-6 flex-grow">
               <HeighAnimation />
             </div>
-
-            <div className="w-full px-6 py-4 border-t border-gray-300 transition-max-height duration-300 ease-in-out  overflow-auto">
-              <div className="flex items-start justify-between">
-                <div>
-                  <div className="text-lg font-semibold text-muted-foreground">
-                    Animating Height
-                  </div>
-                  <p className="font-light opacity-70">
-                    Click the button to animate the height of the box. This animation was built using Framer Motion.
-                  </p>
-                </div>
-                <div className="shrink-0">
-                  <img className="border border-gray-200 rounded-lg" width="45" height="45" alt="Motion logo" src="https://github.com/user-attachments/assets/00d6d1c3-72c4-4c2f-a664-69da13182ffc" />
-                </div>
-              </div>
+            <div className="px-4 py-4 sm:px-6 bg-gray-100/50">
+              <p className="opacity-70">
+                Click the button to animate the height of the box. This animation was built using Framer Motion.
+              </p>
             </div>
           </div>
 
-          <div className="border border-gray-300 rounded-lg overflow-hidden">
+          <div className="divide-y divide-gray-200 overflow-hidden rounded-lg shadow-sm flex flex-col">
+            <div className="px-4 py-2.5 sm:px-6 flex justify-between items-center">
 
-            <div className="w-full flex justify-center items-center  rounded-t-lg py-6">
-              <HeighAnimation />
-            </div>
-
-            <div className="w-full px-6 py-4 border-t border-gray-300 transition-max-height duration-300 ease-in-out  overflow-auto">
-              <div className="flex items-start justify-between">
-                <div className=" max-w-xs">
-                  <div className="text-lg font-semibold text-muted-foreground">
-                    Animating Height
-                  </div>
-                  <p className="font-light opacity-70 mt-1.5">
-                    Click the button to animate the height of the box. This animation was built using Framer Motion.
-                  </p>
-                </div>
-                <div className="shrink-0">
-                  <img className="border border-gray-200 rounded-lg" width="45" height="45" alt="Motion logo" src="https://github.com/user-attachments/assets/00d6d1c3-72c4-4c2f-a664-69da13182ffc" />
-                </div>
+              <div className="text-lg font-semibold text-muted-foreground">
+                Animate Presence
               </div>
+
+              <div className="shrink-0">
+                <img className="border border-gray-200 rounded-lg" width="45" height="45" alt="Motion logo" src="https://github.com/user-attachments/assets/00d6d1c3-72c4-4c2f-a664-69da13182ffc" />
+              </div>
+
+            </div>
+            <div className="px-4 py-5 sm:p-6 flex-grow">
+              <ButtonSendLink />
+            </div>
+            <div className="px-4 py-4 sm:px-6 bg-gray-100/50">
+              <p className="opacity-70">
+                Click the button to see animate the presence of the button. This animation was built using Framer Motion.
+              </p>
             </div>
           </div>
 
