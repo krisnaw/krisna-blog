bdf5c3da607e8fb474b5fa8b9fafa66eeb8fb2f1
krisna <krisna.w2010@gmail.com>
2025-09-10 15:49:47 +0800

    fix copy

--- components/new-section/explore-section.tsx
+++ components/new-section/explore-section.tsx
@@ -16,7 +16,30 @@ export default function ExploreSection() {
 
       <div className="mt-10">
 
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-8">
+
+          <div className="border border-gray-300 rounded-lg overflow-hidden">
+
+            <div className="w-full flex justify-center items-center  rounded-t-lg py-6">
+              <HeighAnimation />
+            </div>
+
+            <div className="w-full px-6 py-4 border-t border-gray-300 transition-max-height duration-300 ease-in-out  overflow-auto">
+              <div className="flex items-start justify-between">
+                <div>
+                  <div className="text-lg font-semibold text-muted-foreground">
+                    Animating Height
+                  </div>
+                  <p className="font-light opacity-70">
+                    Click the button to animate the height of the box. This animation was built using Framer Motion.
+                  </p>
+                </div>
+                <div className="shrink-0">
+                  <img className="border border-gray-200 rounded-lg" width="45" height="45" alt="Motion logo" src="https://github.com/user-attachments/assets/00d6d1c3-72c4-4c2f-a664-69da13182ffc" />
+                </div>
+              </div>
+            </div>
+          </div>
 
           <div className="border border-gray-300 rounded-lg overflow-hidden">
 
