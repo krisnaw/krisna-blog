ba8dd061c773a77c288310987b1b2085499d0f2f
krisna <krisna.w2010@gmail.com>
2025-09-10 19:38:11 +0800

    fix copy

--- components/new-section/explore-section.tsx
+++ components/new-section/explore-section.tsx
@@ -39,7 +39,7 @@ export default function ExploreSection() {
             </div>
             <div className="px-4 py-4 sm:px-6 bg-blue-100/50">
               <p className="opacity-70">
-                Click the button to animate the height of the box. This animation was built using Framer Motion.
+                Click the button to animate the height of the box.
               </p>
             </div>
           </div>
@@ -61,7 +61,7 @@ export default function ExploreSection() {
             </div>
             <div className="px-4 py-4 sm:px-6 bg-blue-100/50">
               <p className="opacity-70">
-                Click the button to see animate the presence of the button. This animation was built using Framer Motion.
+                Click to see animate the presence of the button.
               </p>
             </div>
           </div>
