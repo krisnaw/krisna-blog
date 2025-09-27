42e0c35153bd681b1d434f3449bec9f7d057f49d
krisna <krisna.w2010@gmail.com>
2025-09-27 18:45:38 +0800

    wip

--- components/section/hero.section.tsx
+++ components/section/hero.section.tsx
@@ -1,6 +1,4 @@
 "use client"
-import BlankSectionInside from "@/components/section/blank-section-inside";
-
 import {motion} from "framer-motion";
 import {resume} from "@/app/data";
 import AboutContent from "@/app/components/about-content";
@@ -34,27 +32,8 @@ export default function HeroSection() {
                 </div>
 
                 <AboutContent />
-
-                <div className="relative min-h-96 overflow-hidden opacity-75">
-
-                   <svg fill="none" className="absolute inset-0 size-full stroke-gray-900/10">
-                      <defs>
-                         <pattern id="pattern-87beeb02-b726-4cd1-be69-ae5bc27986e9" width="10" height="10" x="0"
-                                  y="0" patternUnits="userSpaceOnUse">
-                            <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3"></path>
-                         </pattern>
-                      </defs>
-                      <rect width="100%" height="100%" fill="url(#pattern-87beeb02-b726-4cd1-be69-ae5bc27986e9)"
-                            stroke="none">
-                      </rect>
-                   </svg>
-
-
-
-                </div>
              </div>
 
-             <BlankSectionInside />
 
           </div>
 
