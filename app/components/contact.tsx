9e41921dc62b93daa7a7834f3cab8a0383b5d7e5
krisna <krisna.w2010@gmail.com>
2025-10-01 06:23:00 +0800

    wip

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -20,9 +20,9 @@ export function Contact() {
       {socials.map((social) => (
         <a href={social.url} key={social.url}
            className="transition active:scale-[0.9]
-                    cursor-pointer h-9 gap-1.5 inline-flex items-center rounded-full bg-gray-200 hover:bg-gray-100 px-4 py-1 text-sm font-medium text-gray-600">
+           inset-ring inset-ring-gray-300
+                    cursor-pointer h-9 inline-flex items-center rounded-lg bg-gray-200 hover:bg-gray-100 py-1 px-2.5 text-sm font-medium text-gray-600">
           <social.icon className="size-5 text-gray-600"/>
-          {social.name}
         </a>
       ))}
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,70 @@
-import HeroSection from "@/components/section/hero.section";
+import BlankSection from "@/components/section/blank-section";
+import {Contact} from "@/app/components/contact";
+import {CopyIcon} from "@/components/icon/copy.icon";
 
 export default function Page() {
    return (
-       <div className="h-screen">
-          <HeroSection />
+       <div className="relative lg:grid lg:grid-cols-2">
+         <div className="min-h-screen lg:h-screen items-center flex lg:sticky flex-col lg:top-0">
+
+           <div className="py-24">
+
+             <h2 className="text-2xl sm:text-4xl tracking-wider font-bold uppercase">Krisna Wijaya</h2>
+             <h5 className="text-lg sm:text-2xl mb-4 sm:mb-6">
+               Senior Frontend Engineer
+             </h5>
+
+             <p className="font-light max-w-sm text-gray-500 leading-relaxed mb-6">
+               I create software that&#39;s intuitive and effective, allowing users to focus on what matters
+             </p>
+
+             <div className="mb-4">
+               <Contact />
+             </div>
+
+             <div className="flex item-center ">
+               krisna.w2010@gmail.com
+               <CopyIcon className="size-6" />
+             </div>
+
+
+           </div>
+
+
+           <ul>
+             <li>
+               <a href="#about">
+                 About
+               </a>
+             </li>
+             <li>
+               <a href="#projects">
+                 Project
+               </a>
+             </li>
+
+             <li>
+               <a href="#experience">
+                 Experience
+               </a>
+             </li>
+           </ul>
+
+         </div>
+         <div className="relative items-center flex flex-col bg-gray-100">
+           <div className="w-full">
+             <section id="about" className="bg-pink-200">
+               <BlankSection />
+             </section>
+             <section id="projects" className="bg-green-300">
+               <BlankSection />
+             </section>
+
+             <section id="experience">
+               <BlankSection />
+             </section>
+           </div>
+         </div>
        </div>
    )
 }
