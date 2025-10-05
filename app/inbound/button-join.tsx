7dc49236d03e6a9d033c667526d2cf78cda6bac1
krisna <krisna.w2010@gmail.com>
2025-10-05 20:23:56 +0800

    wip

--- app/inbound/button-join.tsx
+++ app/inbound/button-join.tsx
@@ -12,7 +12,8 @@ export function ButtonJoin() {
   useOnClickOutside(ref, () => setOpen(false));
 
   return (
-    <div className="flex justify-center items-center max-w-sm pb-24">
+    <div className="flex justify-center items-end w-full h-[500px]">
+
 
       <motion.button
         onClick={() => {
@@ -20,35 +21,41 @@ export function ButtonJoin() {
         }}
         type="button"
         layoutId="wrapper"
-        className="relative rounded-xl bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-cyan-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        className="relative bottom-0 rounded-xl bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-cyan-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
         <motion.span layoutId="title">
           Join the Waitlist
         </motion.span>
       </motion.button>
 
-      <AnimatePresence>
+
+      <AnimatePresence mode="sync">
         {open && (
           <motion.div
             layoutId="wrapper"
             className="absolute z-40 flex flex-col"
-            ref={ref}>
-
-
+            ref={ref}
+          >
 
             <div role="dialog" className="rounded-2xl p-6 min-w-xl bg-cyan-100 shadow">
               <div className="flex justify-between items-center">
-                <div className="font-bold text-base">
+                <motion.div
+                  layoutId="title"
+                  className="font-bold text-base">
                   New Message
-                </div>
+                </motion.div>
                 <div>
                   <button
                     onClick={() => setOpen(false)}
                     className="text-gray-500">
                     <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M17.25 6.75L6.75 17.25" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
+                      <path d="M17.25 6.75L6.75 17.25" stroke="currentColor" strokeLinecap="round"
+                            strokeLinejoin="round" strokeWidth="1.5">
 
-                      </path><path d="M6.75 6.75L17.25 17.25" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"></path></svg>
+                      </path>
+                      <path d="M6.75 6.75L17.25 17.25" stroke="currentColor" strokeLinecap="round"
+                            strokeLinejoin="round" strokeWidth="1.5"></path>
+                    </svg>
                   </button>
                 </div>
               </div>
@@ -58,22 +65,22 @@ export function ButtonJoin() {
                   <div className="flex items-center gap-3">
                     <label htmlFor="from" className="text-gray-400 text-sm w-16">From: </label>
                     <input id="from" autoFocus={true}
-                      className="focus-visible:outline-0 active:ring-0 focus:ring-0 px-3 py-2.5 flex-1 caret-blue-500"
-                      name="from" placeholder="your@company" />
+                           className="focus-visible:outline-0 active:ring-0 focus:ring-0 px-3 py-2.5 flex-1 caret-blue-500"
+                           name="from" placeholder="your@company"/>
                   </div>
 
                   <div className="flex items-center gap-3">
                     <label htmlFor="to" className="text-gray-400 text-sm w-16">To: </label>
                     <input id="to" defaultValue="team@inbound.resend.app" disabled
                            className="focus-visible:outline-0 active:ring-0 focus:ring-0 px-3 py-2.5 flex-1"
-                           name="to" placeholder="your@company" />
+                           name="to" placeholder="your@company"/>
                   </div>
 
                   <div className="flex items-center gap-3">
                     <label htmlFor="Subject" className="text-gray-400 text-sm w-16">Subject: </label>
                     <input id="Subject" defaultValue="Add me to the Inbound waitlist" disabled
                            className="focus-visible:outline-0 active:ring-0 focus:ring-0 px-3 py-2.5 flex-1"
-                           name="Subject" placeholder="your@company" />
+                           name="Subject" placeholder="your@company"/>
                   </div>
                 </form>
               </div>
--- app/inbound/page.tsx
+++ app/inbound/page.tsx
@@ -3,7 +3,7 @@ import {ButtonJoin} from "@/app/inbound/button-join";
 export default function Page() {
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <div className="absolute bottom-2">
+      <div className="absolute bottom-5">
         <ButtonJoin />
       </div>
     </div>
