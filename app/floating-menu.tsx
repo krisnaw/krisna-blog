e777a324587f0a8e5eb6b94a6fff727a5712b7a0
krisna <krisna.w2010@gmail.com>
2025-10-07 13:59:12 +0800

    wip

---
+++ app/floating-menu.tsx
@@ -0,0 +1,13 @@
+"use client"
+export default function FloatingMenu() {
+  return (
+    <div className="fixed  z-40 top-10/12 w-full flex items-center justify-center">
+      <div>
+        <button className="text-white px-2.5 py-2.5 border border-gray-500 bg-[#11111180] opacity-70 rounded-full">
+          11111180
+        </button>
+      </div>
+    </div>
+
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/app/about";
 
 import {Plus_Jakarta_Sans} from 'next/font/google'
+import FloatingMenu from "@/app/floating-menu";
 
 // If loading a variable font, you don't need to specify the font weight
 const jakartaSans = Plus_Jakarta_Sans({
@@ -8,17 +9,19 @@ const jakartaSans = Plus_Jakarta_Sans({
   display: 'swap',
 })
 
-
 export default function Page() {
-   return (
-       <div>
+  return (
+    <div className="relative">
 
-         <About />
+      <About/>
 
-         <div className={`h-screen bg-white ${jakartaSans}`}>
-           Whereas disregard and contempt for human rights have resulted
-         </div>
+      <div className={`h-screen bg-white ${jakartaSans}`}>
+        <div className="mx-auto max-w-xl">
+          Whereas disregard and contempt for human rights have resulted
+        </div>
+      </div>
 
-       </div>
-   )
+      <FloatingMenu />
+    </div>
+  )
 }
