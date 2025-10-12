3a13d3cbbc6d9a57e8478fb9fa4de14a0270090f
krisna <krisna.w2010@gmail.com>
2025-10-12 14:49:24 +0800

    Added mdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+"use client"
 import FloatingMenu from "@/app/floating-menu";
+import {motion} from "framer-motion";
 
 export default function Page() {
   return (
@@ -6,29 +8,28 @@ export default function Page() {
 
       <section className="bg-white mx-auto max-w-xl flex-grow">
         <div className="pt-28 w-full">
-          <header className="w-full flex flex-col items-center justify-center">
-            <svg viewBox="0 0 70 88" className="w-20" aria-hidden="true">
-              <path
-                d="m23,63.5c2.9-1.9,6-3.8,9.5-4.2s7.5,1.2,8.6,4.5c1.5,4.2-2.2,8.4-5.8,11-3.4,2.5-7.1,4.6-11.2,5.6-4.1,1-8.6.7-12.3-1.3-4.3-2.4-6.9-7-7.9-11.8s-.4-9.7.4-14.5C7.4,36,14.5,19.7,26.2,7.3c2.5-2.7,6.1-5.4,9.6-4,3.4,1.4,4.2,5.8,4.1,9.5-.3,11.3-3.3,22.4-8.7,32.3-.7,1.2-1.4,2.5-2.6,3.3s-2.8,1.1-4,.4c-2.2-1.4-1.3-4.9.3-7,3.4-4.8,8.2-8.4,13.8-10.4,3.9-1.4,8.6-1.7,11.7.9,2.4,2.1,3.2,5.5,2.7,8.6s-2.2,5.9-4.1,8.4c-1.4,1.8-3.7,3.6-5.6,2.4-2.2-1.3-1.1-4.7.4-6.8,2.7-4,6.3-8.2,11.1-8.8,5.2-.6,10.1,3.3,11.8,8.2s.7,10.3-1.5,15c-4,8.9-12,15.9-21.4,18.7"
-                fill="none" stroke="currentColor" strokeWidth="3" className="AnimatedLogo_animated__zbEyH"></path>
-            </svg>
 
-            <h2 className="text-xl  text-gray-900 mt-6">
-              しずかなインターネット
-            </h2>
-          </header>
 
-          <div className="rounded-3xl border border-gray-200 p-7 sm:flex sm:p-8">
-            <div>sdf</div>
-            <div>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="https://static.sizu.me/images/illust/sl-find-a-way.svg" alt="" className="w-32 xs:w-36" />
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{
+              duration: 1,
+            }}
+            className="rounded-3xl border border-gray-200 p-8 flex">
+            <div className="flex-1">
+              <p className="text-gray-600">
+                Hi there, <br/>
+                Thanks for visiting.
+                This site still in progress.
+              </p>
             </div>
-          </div>
+            <div className="ml-4 shrink-0">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="https://static.sizu.me/images/illust/sl-find-a-way.svg" alt="" className="w-32 xs:w-36"/>
+            </div>
+          </motion.div>
 
-          <p>
-            Use flex-utilities like flex-1 to allow a flex item to grow and shrink as needed, ignoring its initial size:
-          </p>
 
         </div>
       </section>
