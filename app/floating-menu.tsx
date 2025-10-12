f3cde6cfda1dbc74f0e6d7808febb0a7a6cd05a7
krisna <krisna.w2010@gmail.com>
2025-10-12 14:20:15 +0800

    Added mdx

--- app/floating-menu.tsx
+++ app/floating-menu.tsx
@@ -1,12 +1,36 @@
 "use client"
 import {useState} from "react";
 import {AnimatePresence, motion} from "framer-motion";
+import Link from "next/link";
+import {ChevronDownIcon} from "lucide-react";
+
+const OPTIONS = [
+  {
+    url: "/",
+    label: "home"
+  },
+  {
+    url: "/notes",
+    label: "notes"
+  },
+  {
+    url: "/contact",
+    label: "contact"
+  }
+]
 
 export default function FloatingMenu() {
   const [isOpen, setIsOpen] = useState(false);
   return (
 
-    <div className="fixed bottom-14 w-full bg-white">
+    <motion.div
+      initial={{
+        opacity: 0
+      }}
+      animate={{
+        opacity: 1
+      }}
+      className="fixed bottom-14 w-full bg-white">
       <AnimatePresence>
         {isOpen && (
           <motion.div
@@ -14,19 +38,40 @@ export default function FloatingMenu() {
             transition={{
               type: "spring",
               duration: 0.4,
-              bounce: 0
             }}
             className="mx-auto max-w-[240px] absolute inset-x-0 bottom-0 z-30">
-            <div
-              className="h-60 bg-gray-950  rounded-xl text-white">
-              <ul>
-                <li>Container menu</li>
-                <li>Container menu</li>
-              </ul>
-              <button onClick={() => setIsOpen(false)}
-                      className="bg-gray-600 w-full text-center">
-                Close
-              </button>
+            <div className="min-h-[100px] bg-gray-950  rounded-lg text-white">
+
+              <div className="p-1.5">
+                <motion.ul
+                  role="list"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.8 }}
+                  className="space-y-1 pt-2.5"
+                >
+                  {OPTIONS.map((option) => (
+                    <li key={option.label}
+                        className="py-3 px-2.5">
+                      <Link href={option.url} className="text-lg/6 font-semibold capitalize">
+                        {option.label}
+                      </Link>
+                    </li>
+                  ))}
+
+                </motion.ul>
+
+                <div className="mt-4">
+                  <button onClick={() => setIsOpen(false)}
+                          className="
+                          bg-neutral-700/70
+                          outline-neutral-500 outline-1
+                          w-full rounded-lg py-2.5 flex items-center justify-center">
+                    <ChevronDownIcon />
+                  </button>
+                </div>
+              </div>
+
             </div>
           </motion.div>
         )}
@@ -37,13 +82,13 @@ export default function FloatingMenu() {
           layoutId="wrapper"
           key="button"
           onClick={() => setIsOpen(true)}
-          className="w-full bg-gray-950/50 hover:bg-gray-950 rounded-xl px-4 py-2.5 text-white text-center cursor-pointer">
+          className="w-full bg-gray-950/50 hover:bg-gray-950 rounded-lg px-4 py-2.5 text-white text-center cursor-pointer">
           <motion.div layout="position">
             Menu
           </motion.div>
         </motion.button>
       </div>
-    </div>
+    </motion.div>
 
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,39 @@ import FloatingMenu from "@/app/floating-menu";
 
 export default function Page() {
   return (
-    <div className="relative h-screen overflow-y-scroll snap-y snap-mandatory">
+    <div className="relative h-screen overflow-y-scroll snap-y snap-mandatory flex flex-col">
 
-      <section className="bg-sky-200 h-screen">
-        Hero section
+      <section className="bg-white mx-auto max-w-xl flex-grow">
+        <div className="pt-28 w-full">
+          <header className="w-full flex flex-col items-center justify-center">
+            <svg viewBox="0 0 70 88" className="w-20" aria-hidden="true">
+              <path
+                d="m23,63.5c2.9-1.9,6-3.8,9.5-4.2s7.5,1.2,8.6,4.5c1.5,4.2-2.2,8.4-5.8,11-3.4,2.5-7.1,4.6-11.2,5.6-4.1,1-8.6.7-12.3-1.3-4.3-2.4-6.9-7-7.9-11.8s-.4-9.7.4-14.5C7.4,36,14.5,19.7,26.2,7.3c2.5-2.7,6.1-5.4,9.6-4,3.4,1.4,4.2,5.8,4.1,9.5-.3,11.3-3.3,22.4-8.7,32.3-.7,1.2-1.4,2.5-2.6,3.3s-2.8,1.1-4,.4c-2.2-1.4-1.3-4.9.3-7,3.4-4.8,8.2-8.4,13.8-10.4,3.9-1.4,8.6-1.7,11.7.9,2.4,2.1,3.2,5.5,2.7,8.6s-2.2,5.9-4.1,8.4c-1.4,1.8-3.7,3.6-5.6,2.4-2.2-1.3-1.1-4.7.4-6.8,2.7-4,6.3-8.2,11.1-8.8,5.2-.6,10.1,3.3,11.8,8.2s.7,10.3-1.5,15c-4,8.9-12,15.9-21.4,18.7"
+                fill="none" stroke="currentColor" strokeWidth="3" className="AnimatedLogo_animated__zbEyH"></path>
+            </svg>
+
+            <h2 className="text-xl  text-gray-900 mt-6">
+              しずかなインターネット
+            </h2>
+          </header>
+
+          Hero section
+        </div>
       </section>
 
-      <section className="bg-amber-200 h-screen">
-        Feature section
-      </section>
 
-      <FloatingMenu />
+      <footer className="bg-blue-200/50">
+        <div className="mx-auto max-w-xl px-6 pt-16 pb-8 sm:pt-24 lg:px-8 lg:pt-32">
+          <div className="grid grid-cols-3">
+            sdf
+          </div>
+        </div>
+      </footer>
+
+
+      <FloatingMenu/>
+
+
 
     </div>
   )
