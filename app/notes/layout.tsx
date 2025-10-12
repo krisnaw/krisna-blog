a72d77ea587e7e52befede707f8ef3c89f791655
krisna <krisna.w2010@gmail.com>
2025-10-12 21:24:03 +0800

    Added mdx

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,48 +1,9 @@
 export default function NotesLayout({children}: { children: React.ReactNode }) {
   return (
-    <div className="w-full min-h-screen bg-gradient-to-t from-sky-100/20 via-sky-200 to-sky-200/50">
-      <header>
-        <nav className="px-4 sm:px-6 lg:px-8 py-2.5">
-          <ul>
-            <li>
-              {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-              <a href="/notes">
-                Home
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      {children}
-
-      <footer className="mx-auto max-w-sm pt-12 pb-10">
-        <ul className="flex justify-center items-center space-x-4.5 [&>li]:text-gray-500 [&>li]:font-semibold">
-          <li>
-            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-            <a href="/notes">
-              Footer
-            </a>
-          </li>
-
-          <li>
-            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-            <a href="/notes">
-              Footer
-            </a>
-          </li>
-
-          <li>
-            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-            <a href="/notes">
-              Footer
-            </a>
-          </li>
-        </ul>
-        <div className="text-center mt-6 font-medium text-gray-500">
-          ©{" "}  {new Date().getFullYear()} Krisna Wijaya
-        </div>
-      </footer>
+    <div className="bg-pink-200 flex flex-col h-screen">
+      <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 flex-grow">
+        {children}
+      </div>
     </div>
   )
 }
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,18 +1,9 @@
-import {BlogPosts} from "@/app/notes/posts";
-
 export default function Page() {
   return (
 
-    <div className="py-20">
-      <div className="mx-auto max-w-xl py-12">
-        <div>
-          <div className="text-3xl font-semibold">
-            Notes
-          </div>
-          <div className="mt-10">
-            <BlogPosts />
-          </div>
-        </div>
+    <div>
+      <div className="text-3xl font-semibold">
+        Notes
       </div>
     </div>
 
