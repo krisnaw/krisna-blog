40752fcaded393443ae32e4784e3b789d4e57618
krisna <krisna.w2010@gmail.com>
2025-10-13 09:26:51 +0800

    Added mdx

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,3 +1,14 @@
+const NOTES = [
+  {
+    id: crypto.randomUUID(),
+    slug: "book",
+    title: "Book I've read",
+    date: "13 October 2025",
+    excerpt: "Communication comes first. High‑quality results depend on both individual skill and\n" +
+      "                  seamless collaboration with directors, designers, and the rest of the team."
+  }
+];
+
 export default function Page() {
   return (
     <div>
@@ -7,32 +18,29 @@ export default function Page() {
       <div className="mt-10">
         <ul role="list" className="divide-y divide-gray-100 space-y-2.5">
 
-          {Array.from({ length: 10 }).map((_, index) => (
-            <li key={index} className="pb-4">
+          {NOTES.map((note,) => (
+            <li key={note.id} className="pb-4">
               <div>
                 <h3 className="text-lg font-semibold text-gray-900">
-                  <a href={`/notes/sample`} className="hover:underline">
-                    How to build a successful team
+                  <a href={`/notes/${note.slug}`} className="hover:underline">
+                    { note.title }
                   </a>
                 </h3>
               </div>
 
               <div>
                 <p className="text-sm text-gray-500">
-                  Communication comes first. High‑quality results depend on both individual skill and
-                  seamless collaboration with directors, designers, and the rest of the team.
+                  {note.excerpt}
                 </p>
               </div>
 
               <div className="mt-2 flex justify-between">
                 <div className="text-sm text-gray-500">
-                  <time dateTime="2022-01-01">January 1, 2022</time>
+                  <time dateTime={note.date}>{note.date}</time>
                 </div>
               </div>
             </li>
           ))}
-
-
         </ul>
       </div>
     </div>
