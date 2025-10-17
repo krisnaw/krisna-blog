816dc9b33591ac2137df36b9e556d10d111caeb6
krisna <krisna.w2010@gmail.com>
2025-10-17 21:02:58 +0800

    wip

---
+++ app/notes/NOTES.tsx
@@ -0,0 +1,25 @@
+type Note = {
+  id: string;
+  slug: string;
+  title: string;
+  date: string;
+  excerpt: string;
+}
+
+export const NOTES: Note[] = [
+  {
+    id: crypto.randomUUID(),
+    slug: "scroll-driven-animation",
+    title: "CSS Scroll driven animation",
+    date: "16 October 2025",
+    excerpt: "CSS scroll-driven animations module provides functionality that builds on top of the CSS animations module and Web Animations API.",
+  },
+  {
+    id: crypto.randomUUID(),
+    slug: "book",
+    title: "Book I've read",
+    date: "13 October 2025",
+    excerpt: "Communication comes first. High‑quality results depend on both individual skill and\n" +
+      "                  seamless collaboration with directors, designers, and the rest of the team."
+  }
+];
