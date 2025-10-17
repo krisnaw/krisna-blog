0180728559630a0c7b753f8840a6c2251a95946d
krisna <krisna.w2010@gmail.com>
2025-10-17 19:51:37 +0800

    wip

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function NotesLayout({children}: { children: React.ReactNode }) {
   return (
-    <div className={`${merriweather.className} bg-slate-300 h-screen`}>
+    <div className={`${merriweather.className} bg-neutral-100/50 h-screen`}>
       {children}
     </div>
   )
