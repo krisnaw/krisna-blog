9eb1e6955925c69db7e346bb59e522778a43897d
krisna <krisna.w2010@gmail.com>
2025-10-18 14:52:14 +0800

    wip

---
+++ app/notes/(post)/header.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import {PostType} from "@/app/notes/getPosts";
+import {useSelectedLayoutSegments} from "next/navigation";
+
+export function Header({posts} : {posts: PostType[]}) {
+  const segments = useSelectedLayoutSegments()
+  const initialPost = posts.find(
+    post => post.id === segments[segments.length - 1]
+  );
+
+  console.log(initialPost)
+
+  return (
+    <div>
+      <h1 className="text-3xl mb-1 font-bold">{initialPost?.title}</h1>
+      <div>
+        {initialPost?.date}
+      </div>
+    </div>
+  )
+}
--- app/notes/(post)/layout.tsx
+++ app/notes/(post)/layout.tsx
@@ -1,12 +1,15 @@
 import {Suspense} from "react";
 import BackButton from "@/app/notes/(post)/back-button";
 import {MainContent} from "@/app/notes/(post)/main-content";
+import {getPosts} from "@/app/notes/getPosts";
+import {Header} from "@/app/notes/(post)/header";
 
 export default async function MdxLayout({ children }: { children: React.ReactNode }) {
-
+  const posts = getPosts();
   return (
     <div className={`relative bg-slate-50`}>
-      <header className="z-30 lg:sticky lg:top-0">
+
+      <nav className="z-30 lg:sticky lg:top-0">
 
         <div className="px-4 sm:px-6 lg:px-8 h-14 py-4">
           <Suspense>
@@ -14,10 +17,11 @@ export default async function MdxLayout({ children }: { children: React.ReactNod
           </Suspense>
         </div>
 
-      </header>
+      </nav>
 
-      <div className="py-20 mx-auto max-w-3xl">
-        <div className="py-10 px-4 sm:px-6 lg:px-8 bg-white rounded-3xl shadow-md">
+      <div className="pt-6 pb-60 mx-auto max-w-3xl">
+        <Header posts={posts} />
+        <div className="mt-6">
           <Suspense fallback={<div>Loading</div>}>
             <MainContent>{children}</MainContent>
           </Suspense>
