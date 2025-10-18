88f9abd255a915737f8589d5164173053b278a47
krisna <krisna.w2010@gmail.com>
2025-10-18 17:05:20 +0800

    wip

--- app/notes/(post)/og/[id]/route.tsx
+++ app/notes/(post)/og/[id]/route.tsx
@@ -2,7 +2,7 @@ export const revalidate = 300;
 import {ImageResponse} from "next/og";
 
 export async function GET() {
-
+  console.log("OG IMAGE")
   return new ImageResponse(
     <div>
       This is og image
