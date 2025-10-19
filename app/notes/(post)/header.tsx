c54b8d1fe6d0a7b7fd745a6ee2f8f0cbbc2884f8
krisna <krisna.w2010@gmail.com>
2025-10-19 14:04:18 +0800

    fixed layout

--- app/notes/(post)/header.tsx
+++ app/notes/(post)/header.tsx
@@ -3,6 +3,7 @@
 import {PostType} from "@/app/notes/getPosts";
 import {useSelectedLayoutSegments} from "next/navigation";
 import Image from "next/image";
+import {motion} from "framer-motion";
 
 export function Header({posts} : {posts: PostType[]}) {
   const segments = useSelectedLayoutSegments()
@@ -12,19 +13,22 @@ export function Header({posts} : {posts: PostType[]}) {
 
   return (
     <div className="text-center">
-      <h1 className="text-3xl mb-1 font-bold">{initialPost?.title}</h1>
-      <div className="text-gray-600">
-        {initialPost?.date}
-      </div>
+      <motion.div
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+      >
+        <h1 className="text-3xl mb-1 font-bold">{initialPost?.title}</h1>
+        <div className="text-gray-600">
+          {initialPost?.date}
+        </div>
+      </motion.div>
       <div className="pt-6">
-
         { initialPost?.featuredImage && (
           <Image
             quality={100}
             className="h-44 sm:h-96 w-full object-cover  grayscale outline-1 outline-gray-300"
             src={initialPost?.featuredImage} width={800} height={400} alt="Hello World" />
         )}
-
       </div>
     </div>
   )
