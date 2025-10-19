6eb4439bc54b13ecda08159d3631aae7421cf24a
krisna <krisna.w2010@gmail.com>
2025-10-19 17:49:10 +0800

    fixed layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/section/hero.section";
 import React from "react";
 import FooterSection from "@/components/section/footer.section";
-import {LinearMenu} from "@/app/(sketch)/linear-menu";
 
 export default function Page() {
   return (
@@ -10,7 +9,27 @@ export default function Page() {
         <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
           <HeroSection/>
 
-          <LinearMenu />
+          <div>
+            <div className="mb-6">
+              Experiences
+            </div>
+
+            <div>
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-[1fr_2fr]">
+                <div>
+                  <div className="text-lg font-medium text-gray-500">2024-Now</div>
+                </div>
+                <div>
+                  <h4 className="text-lg font-medium text-gray-950">
+                    Senior Product Designer at Atmosphere TV
+                  </h4>
+                  <p className="text-gray-500">
+                    Lead designer for tv for bars & restaurants
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
 
         </div>
       </div>
