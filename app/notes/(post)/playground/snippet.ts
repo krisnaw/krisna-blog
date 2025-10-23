14a11681c037f6231b71bb40f842c687ebbc224b
krisna <krisna.w2010@gmail.com>
2025-10-23 14:12:15 +0800

    wip code block

---
+++ app/notes/(post)/playground/snippet.ts
@@ -0,0 +1,6 @@
+export const exampleCode = `const greeting = "Hello, Next.js!";
+console.log(greeting);
+// Add more complex code here if needed
+function example() {
+  return greeting.toUpperCase();
+}`;
