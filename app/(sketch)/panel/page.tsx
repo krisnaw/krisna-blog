a6eca056d2f5d9668f04946d911f9c517f4ba462
krisna <krisna.w2010@gmail.com>
2025-11-03 13:33:26 +0800

    wip code block

---
+++ app/(sketch)/panel/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-zinc-900">
+
+
+      <div className="h-96 w-xl rounded-2xl bg-zinc-800/60 text-white shadow shadow-white/50 inset-shadow inset-shadow-white p-2.5">
+        <div className="flex items-center justify-center h-full bg-zinc-800 rounded-xl inset-shadow-sm inset-shadow-zinc-500">
+          Panel
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
--- components/code-tab/code-panel.tsx
+++ components/code-tab/code-panel.tsx
@@ -1,6 +1,11 @@
 import {BundledLanguage, BundledTheme, codeToHtml} from "shiki";
 import {transformerNotationHighlight} from "@shikijs/transformers";
-import ButtonCopyCode from "@/components/code-tab/button-copy-code";
+import {useEffect, useState} from "react";
+import {toast} from "sonner";
+import {AnimatePresence, motion} from "framer-motion";
+import {CheckIcon, Copy} from "lucide-react";
+
+const SIZE = 16;
 
 type Props = {
   code: string;
@@ -9,7 +14,7 @@ type Props = {
   theme?: BundledTheme;
 }
 
-export default async function CodePanel({code, filename, lang = "jsx", theme = "github-light"}: Props) {
+async function CodePanel({code, filename, lang = "jsx", theme = "github-light"}: Props) {
   if (!code) return null
 
   const html = await codeToHtml(code, {
@@ -19,18 +24,9 @@ export default async function CodePanel({code, filename, lang = "jsx", theme = "
   })
 
   return (
-    <div className="not-prose rounded-md outline outline-gray-300 min-w-xl">
-
-      <div className="p-2 bg-gray-100/70 border-b border-gray-200 rounded-t-md">
-        <div className="flex justify-between items-center w-full">
-          <div>
-            {/*empty for now*/}
-          </div>
-          <div>
-            <ButtonCopyCode code={code} />
-          </div>
-        </div>
-      </div>
+    <div
+      data-slot="code-panel"
+      className="not-prose rounded-md outline outline-gray-300 min-w-xl">
 
       <div className="p-4">
         <div dangerouslySetInnerHTML={{ __html: html }} />
@@ -38,4 +34,81 @@ export default async function CodePanel({code, filename, lang = "jsx", theme = "
     </div>
 
   )
+}
+
+function CodeProvider() {
+  return (
+    <div>sdf</div>
+  )
+}
+
+function CodeHeader({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="code-header"
+      className="p-2 bg-gray-100/70 border-b border-gray-200 rounded-t-md"{...props}
+    />
+  )
+}
+
+function CodeDisplay() {
+  return (
+    <div>sdf</div>
+  )
+}
+
+function CodeCopy({code} : {code: string}) {
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (copied) {
+      const timeout = setTimeout(() => {
+        setCopied(false);
+      }, 1500);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [copied])
+
+  const onClickHandler = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(code);
+    toast.success("Copied to clipboard")
+  }
+
+  return (
+    <motion.button
+      whileTap={{ scale: 0.8 }}
+      transition={{ type: "spring", duration: 0.5, bounce: 0 }}
+      className="
+      cursor-pointer
+      shrink-0
+      p-1.5
+      inline-flex items-center justify-center rounded-md
+      border bg-background shadow-xs
+      hover:bg-gray-100
+      text-gray-500"
+      onClick={onClickHandler} >
+      <AnimatePresence mode="popLayout" initial={false}>
+        <motion.div
+          key={copied ? "copied" : "copy"}
+          initial={{ opacity: 0, scale: 0.25, filter: "blur(4px)" }}
+          animate={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
+          exit={{ opacity: 0, scale: 0.25, filter: "blur(4px)" }}
+          transition={{
+            type: "spring",
+            duration: 0.3,
+            bounce: 0,
+          }}
+        >
+          {copied ? <CheckIcon size={SIZE} /> :  <Copy size={SIZE} />}
+        </motion.div>
+      </AnimatePresence>
+    </motion.button>
+  )
+}
+
+export {
+  CodePanel,
+  CodeHeader,
 }
