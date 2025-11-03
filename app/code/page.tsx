f2dc69223bf353dbc995e43c7610e5a6d4d57f6f
krisna <krisna.w2010@gmail.com>
2025-11-03 14:06:25 +0800

    wip code block

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -1,15 +1,9 @@
-import CodePanel from "@/components/code-tab/code-panel";
 
 export default async function Page() {
 
   return (
     <div className="h-screen mx-auto max-w-lg flex items-center justify-center">
 
-      <CodePanel
-        code={`fn main() { println!("Hello, world!"); }`}
-        lang="tsx"
-        filename="/index.tsx"
-      />
 
     </div>
   )
