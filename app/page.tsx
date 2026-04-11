b66c8bf19ea9202fe2b080899fd915f8ce3b3ad9
krisna <krisna.w2010@gmail.com>
2026-04-11 11:40:22 +0800

    Updated to Next.js 16.2

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,23 @@ import HeroSection from "@/components/section/hero.section";
 import React from "react";
 import FooterSection from "@/components/section/footer.section";
 import WorksSection from "@/components/section/works.section";
-import {CertSection} from "@/components/section/cert.section";
+import { CertSection } from "@/components/section/cert.section";
 
 export default function Page() {
   return (
     <div className="flex flex-col h-svh">
-      <div className="flex-grow">
+      <div className="grow">
         <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 py-24 sm:py-36">
-          <HeroSection/>
+          <HeroSection />
           <WorksSection />
           <CertSection />
         </div>
       </div>
-      <FooterSection/>
+      <FooterSection />
+      <div>
+        Nothing but the beat
+        <h1 className="text-2xlx"></h1>
+      </div>
     </div>
-
-  )
+  );
 }
