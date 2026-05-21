9231d9ef13f3284abeae4ef7aec9d13d1b00b6c5
krisna <krisna.w2010@gmail.com>
2026-05-21 11:09:14 +0800

    wip web update

--- components/project.tsx
+++ components/project.tsx
@@ -10,24 +10,29 @@ export function Projects() {
       <div className="mb-5">
         <ProjectHeading>Latest Projects</ProjectHeading>
         <ProjectSubheading>
-          Learn how to grow your business with our expert advice.
+          A glimpse into my recent product that I've build. 
         </ProjectSubheading>
       </div>
       <div className="grid w-full grid-cols-1 gap-4">
 
         {projects.map((project: Project) => (
           <div className="group rounded-lg outline outline-gray-200 pt-2.5 bg-stone-100" key={project.id}>
-            <div className="h-14 px-4 ">
-              <h3 className="text-base font-semibold text-gray-900">
-                <Link href={project.link}>{project.name}</Link>
-              </h3>
+            <div className="min-h-14 px-4 pb-2.5">
+              <div className="flex justify-between">
+                <h3 className="text-base font-semibold text-gray-600">
+                  <Link href={project.link}>{project.name}</Link>
+                </h3>
+                <p className="text-gray-400 text-sm">
+                  {project.year}
+                </p>
+              </div>
               <p className="mt-1 text-sm text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit quam corrupti consectetur.
+                {project.description}
               </p>
             </div>
             <div className="bg-white outline outline-gray-200 rounded-lg">
               <div className="flex justify-center items-center w-full">
-                <Image src="/GolangLogo.webp" width={400} height={400} alt="golang" />
+                <Image src="/GolangLogo.webp" width={350} height={350} alt="golang" />
               </div>
             </div>
           </div>
@@ -43,15 +48,17 @@ const projects : Project[] = [
   {
     id: 1,
     name: "Barong Melali",
-    description: "",
+    description: "An event management and registration web app built for Barong Cycling Team, the largest road cycling group in Bali",
     image: "",
     link: "/project/barong-melali",
+    year: '2026'
   },
   {
     id: 2,
     name: "BetterInvoice",
-    description: "",
+    description: "An Invoice management app build for Freelancer in Indonesia.",
     image: "",
     link: "",
+    year: '2026'
   },
 ]
--- lib/types.ts
+++ lib/types.ts
@@ -4,4 +4,5 @@ export type Project = {
   description: string,
   image: string,
   link: string
+  year: string
 }
