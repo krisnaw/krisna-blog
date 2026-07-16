66d4418c269550e5163e76ddd86796587ff6290e
krisna <krisna.w2010@gmail.com>
2026-07-16 21:40:02 +0800

    added typeset

--- app/notes/(post)/layout.tsx
+++ app/notes/(post)/layout.tsx
@@ -1,34 +1,41 @@
 import {getPosts} from "@/app/notes/getPosts"
 import Link from "next/link";
 import {Header} from "@/app/notes/(post)/header";
+import {DirectionalTransition} from "@/components/view-transition";
 
 export default async function MdxLayout({children}: {children: React.ReactNode}) {
   const posts = getPosts()
 
   return (
-    <div className="min-h-screen bg-zinc-50 pb-20 pt-28 dark:bg-zinc-950">
+    <DirectionalTransition>
+      <div className="min-h-screen bg-zinc-50 pb-20 pt-28 dark:bg-zinc-950">
 
-      <div className="mx-auto max-w-3xl">
+        <div className="mx-auto max-w-3xl">
 
-        <div>
-          <Link href="/notes" className="text-sm text-zinc-600 dark:text-zinc-400">
-            ← Notes
-          </Link>
-        </div>
-
-        <div className="mt-4 rounded-xl border border-zinc-100 bg-white px-20 py-10 shadow-lg dark:border-zinc-800 dark:bg-zinc-900">
-          <div className="mx-auto">
-            <div className="typeset typeset-docs">
-              <Header posts={posts} />
-
-              <article className="text-zinc-600 dark:text-zinc-400">
-                {children}
-              </article>
-            </div>
+          <div>
+            <Link
+              href="/notes"
+              transitionTypes={["nav-back"]}
+              className="text-sm text-zinc-600 dark:text-zinc-400"
+            >
+              ← Notes
+            </Link>
           </div>
 
+          <div className="mt-4 rounded-xl border border-zinc-100 bg-white px-20 pt-6 pb-18 shadow-lg dark:border-zinc-800 dark:bg-zinc-900">
+            <div className="mx-auto">
+              <div className="typeset typeset-docs">
+                <Header posts={posts} />
+
+                <article className="text-zinc-600 dark:text-zinc-400">
+                  {children}
+                </article>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
-    </div>
+    </DirectionalTransition>
   )
 }
